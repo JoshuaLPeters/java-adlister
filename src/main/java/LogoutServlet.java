1c4f9bff3b9a7d84d46a1ed375dcdc0a0736a0c2
Joshua Peters <peters.l.joshua@gmail.com>
2020-01-13 15:06:36 -0600

    saving session exercise complete

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -6,15 +6,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Object registeredUser = request.getSession().getAttribute("user");
 
-        if (registeredUser != null) {
-                request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("/login");
-        }
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("/login");
+
     }
+
+
 }
